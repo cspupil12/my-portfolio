ad624ae7dcfd3e5936239996c431d87e476e0736
cspupil12 <jaspreet.singh.para@gmail.com>
2026-07-24 13:44:22 +0530

    Removed all social links

--- inc/footer.php
+++ inc/footer.php
@@ -1,17 +1,8 @@
 <div class="conor_tm_footer_wrap">
     <div class="container">
         <div class="inner_wrap">
-            <div class="copyright">
+            <div class="copyright" style="text-align: center; width: 100%;">
                 <p>Copyright &copy; 2019. All Rights are Reserved.</p>
-            </div>
-            <div class="social">
-                <ul>
-                    <li><a href="#"><i class="xcon-facebook"></i></a></li>
-                    <li><a href="#"><i class="xcon-twitter"></i></a></li>
-                    <li><a href="#"><i class="xcon-instagram"></i></a></li>
-                    <li><a href="#"><i class="xcon-linkedin"></i></a></li>
-                    <li><a href="#"><i class="xcon-behance"></i></a></li>
-                </ul>
             </div>
         </div>
     </div>
